e644c3e5c7fab7fb719d1647b688df99ec57f379
antonytvton <99509765+antonytvton@users.noreply.github.com>
2022-11-05 11:57:43 -0700

    first commit

---
+++ definecraft.py
@@ -0,0 +1,81 @@
+#definecraft
+import pygame
+import math
+from numpy import *
+global Foxtrot
+Foxtrot = {"maxspeed": 2750, "turnrate": 30, "acceleration": 600, "image": "foxtrot1.png", "largehardpoint": 0, "smallhardpoint": 0, "gunhardpoint": 0, "highlighted_image": "foxtrothighlighted1.png", "highlightoverlay" : "outline.png"}
+
+def rot_center(image, angle, cent):
+    """rotate a Surface, maintaining position."""
+    
+    loc = cent.get_rect().center  #rot_image is not defined 
+    rot_sprite = pygame.transform.rotate(image, angle)
+    rot_sprite.get_rect().center = loc
+    return rot_sprite
+
+
+
+class Plane(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super(Plane, self).__init__()
+        self.surf = pygame.image.load(type["image"]).convert_alpha()
+
+
+        self.surf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
+
+        self.surf = pygame.transform.smoothscale(self.surf, (30, 30))
+        
+
+class Craft:
+
+
+      def __init__(self, type, team, name, startx = 0, starty = 0):
+            #defining each craft type
+
+
+            #inital data about craft
+            self.maxspeed = type["maxspeed"]
+            self.type = type
+            self.acceleration = type["acceleration"]
+            self.turnrate = type["turnrate"]
+            self.name = name
+            self.team = team
+            self.target_angle = 0
+            self.target = [0, 0]
+
+            #positional and movement data about craft
+            self.xpos = startx
+            self.ypos = starty
+            self.curspeed = self.maxspeed
+            self.angle = -130
+            #init craft sprite
+            self.sprite = Plane(type)
+
+
+      def time_tick(self, selectedobjects, screen, timeish):
+            self.ypos = (sin(deg2rad(self.angle)))*(self.curspeed/200000)*70 + self.ypos
+            self.xpos = -(cos(deg2rad(self.angle)))*(self.curspeed/200000)*70 + self.xpos
+            if (self.xpos - self.target[0]) > 0:
+                  self.target_angle = -(rad2deg(real(math.atan((self.ypos - self.target[1])/(self.xpos - self.target[0])))))
+
+            if (self.xpos - self.target[0]) < 0:
+                  self.target_angle = -(rad2deg(real(math.atan((self.ypos - self.target[1])/(self.xpos - self.target[0])))))+180
+
+            self.target_angle = self.target_angle % 360
+            self.differential = self.target_angle - self.angle
+            self.differential = self.differential % 360
+            if self.differential % 360 < 180:
+                  self.angle += +self.turnrate/60
+
+
+            if self.differential % 360 > 180:
+                  self.angle += -self.turnrate/60
+
+            if selectedobjects.count(self) == 1:
+                  self.outline = pygame.image.load("outline.png").convert_alpha()
+                  #self.outline.set_colorkey((255, 255, 255), pygame.RLEACCEL)
+                  self.outline =  rot_center(self.outline, timeish, self.sprite.surf)
+                  screen.blit(self.outline, (self.xpos - 10, self.ypos - 8))
+                  
+
+                  
---
+++ main.py
@@ -0,0 +1,145 @@
+#main
+import time
+import sys
+import pygame
+pygame.init()
+from extra_libraries import *
+from definecraft import *
+from pygame.locals import (
+      RLEACCEL, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE
+      , KEYDOWN, QUIT)
+
+pygame.init()
+clock = pygame.time.Clock()
+screen_w = 1900
+screen_h = 1000
+global selectedobjects
+lc = 0
+rc = 0
+rightclick = False
+leftclick = False
+# Set up the drawing window
+screen = pygame.display.set_mode([screen_w, screen_h])
+allcraftsprite = []
+bg = pygame.image.load("background.png")
+
+global ticktime
+#init craft
+global timeish
+timeish = 0
+foxtrot1 = Craft(Foxtrot, 1, "foxtrot1", 200, 800)
+foxtrot2 = Craft(Foxtrot, 1, "foxtrot1", 400, 800)
+foxtrot3 = Craft(Foxtrot, 1, "foxtrot1", 600, 800)
+
+
+allcraft = [foxtrot1, foxtrot2, foxtrot3]
+selectedobjects = []
+for i in allcraft:
+      allcraftsprite.append(i.sprite)
+
+
+def rot_center(image, angle):
+    """rotate a Surface, maintaining position."""
+    
+    loc = image.get_rect().center  #rot_image is not defined 
+    rot_sprite = pygame.transform.rotate(image, angle)
+    rot_sprite.get_rect().center = loc
+    return rot_sprite
+
+# Run until the user asks to quit
+
+while True:
+
+      # Did the user click the window close button?
+      for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                  pygame.quit()
+                  sys.exit
+
+      #determine if leftclicking or rightclicking
+      mouse_pressed = pygame.mouse.get_pressed()
+      if mouse_pressed[0] == True and mouse_pressed[2] == False:
+            if lc == 1:
+                  lc = 2
+                  leftclick = False
+            if lc == 2:
+                  lc = 2
+                  leftclick = False
+            if lc == 0:
+                  lc = 1
+                  leftclick = True
+
+      else:
+            lc = 0
+            leftclick = False
+
+      if mouse_pressed[2] == True and mouse_pressed[0] == False:  
+            if rc == 1:
+                  rc = 2
+                  rightclick = False
+            if rc == 2:
+                  lc = 2
+                  rightclick = False
+            if rc == 0:
+                  rc = 1
+                  rightclick = True
+
+      else:
+            rc = 0 
+            rightclick = False   
+
+
+
+      if leftclick == True:
+            mods = pygame.key.get_mods()
+            if selectedobjects == [] or mods & pygame.KMOD_SHIFT:
+
+                  for plane in allcraft:
+                        if (sqrt(square(plane.xpos - (pygame.mouse.get_pos()[0])) + square(plane.ypos - (pygame.mouse.get_pos()[1]))) < 35) and selectedobjects.count(plane) == 0:
+
+                              selectedobjects.append(plane)
+            else:
+                  for plane in selectedobjects:
+                        plane.target = pygame.mouse.get_pos()
+      if rightclick == True:
+            selectedobjects = []
+
+      # clear screen
+      screen.fill((0, 0, 0))
+      
+
+
+      screen.blit(bg, (0, 0))  
+      #you can edit the screen from here on out
+      
+      for plane in allcraft:
+            plane.sprite.surf = pygame.image.load(plane.type["image"]).convert_alpha()
+            plane.sprite.surf = pygame.transform.smoothscale(plane.sprite.surf, (30, 19))
+      
+
+      for plane in selectedobjects:
+            plane.sprite.surf = pygame.image.load(plane.type["highlighted_image"]).convert_alpha()
+            plane.sprite.surf = pygame.transform.smoothscale(plane.sprite.surf, (30, 19))
+
+
+
+      for plane in allcraft:
+            plane.time_tick(selectedobjects, screen, timeish)
+
+      for plane in allcraft:
+            draw_dashed_line(screen, (42, 163, 2), plane.target, (plane.xpos+15, plane.ypos+17), 2)
+
+
+
+      for plane in allcraft:
+            screen.blit(rot_center(plane.sprite.surf, plane.angle), (plane.xpos, plane.ypos))
+
+            #YES I do have a reason for spliting these into two sepperate opperations. It is to control the priority that items are created in to contol what layers they are on
+            #The priority of these items is as folows >plane >dashed line >radar thing that goes around and highlits stuff
+
+
+
+
+      pygame.display.flip()
+      timeish += 1
+      (clock.tick(60))
